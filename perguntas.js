b0b69ef7d9e5257ca2327cf45bea686aa502eb26
SueCzek <161771852+SueCzek@users.noreply.github.com>
2025-05-22 17:45:19 -0300

    Add files via upload

--- perguntas.js
+++ perguntas.js
@@ -1,5 +1,31 @@
 criaCartao(
-    categoria,
-    pergunta,
-    resposta
+    'Programação',
+    'Qual comando é utilizado para alterar o parágrafo do rodapé (footer)?',
+    'font-size: 0,5 rem;'
 )
+criaCartao(
+    'Programação',
+    'Qual alternativa apresenta a propriedade do CSS que altera a largura de um elemento?',
+    'width: 100%;'
+)
+criaCartao(
+    'Programação',
+    'A tag article é utilizada para:',
+    'Quando precisamos criar um elemento independente em HTML.'
+)
+criaCartao(
+    'Programação',
+  ' Qual propriedade podemos solicitar que o CSS faça um calculo, para que possamos dividir a tela em 3 cartões?',
+   ' flex-basis:'
+)
+criaCartao(
+    'Programação',
+    'No cartão, no componete curricular, quando queremos que o texto ocupe apenas a posição desejada, aplicaremos:',
+    'position: absolute;'
+)
+criaCartao(
+    'Programação',
+    'Como adicionar uma margem arredondada, aplicamos:',
+    'border-radius:0.6rem;'
+)
+              
